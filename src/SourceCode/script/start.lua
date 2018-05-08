b332d40091f38476bd8e7dc522e228901864b2af
Tricky1975 <facebook@tbbs.nl>
2018-05-08 12:25:16 +0200

    vUpdate

--- src/SourceCode/script/start.lua
+++ src/SourceCode/script/start.lua
@@ -20,7 +20,7 @@
 		
 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
 	to the project the exceptions are needed for.
-Version: 18.05.07
+Version: 18.05.08
 ]]
 
 -- $USE libs/maneschijn
